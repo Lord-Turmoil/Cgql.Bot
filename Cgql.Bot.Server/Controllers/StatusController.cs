5c4691e01879ea096f7e3c1b44e6ed7648ca755a
Lord Turmoil <tony-turmoil@outlook.com>
2024-04-05 23:38:29 +0800

    feat: add status api

---
+++ Cgql.Bot.Server/Controllers/StatusController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Tonisoft.AspExtensions.Module;
+using Tonisoft.AspExtensions.Response;
+
+namespace Cgql.Bot.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class StatusController : BaseController<StatusController>
+{
+    public StatusController(IMapper mapper, ILogger<StatusController> logger) : base(mapper, logger)
+    {
+    }
+
+    [HttpGet]
+    public ApiResponse Ping()
+    {
+        return new OkResponse(new OkDto("Pong"));
+    }
+
+    [HttpGet]
+    public ApiResponse Profile()
+    {
+        return new OkResponse(new OkDto(data: new {
+            Configuration.Profile,
+            Configuration.Version,
+            Configuration.BotId
+        }));
+    }
+}
